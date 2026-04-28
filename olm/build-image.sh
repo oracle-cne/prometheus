080a9ca09e1a2c2830a92939846e9f6723b7dbc6
Oracle Cloud Native Environment Authors <noreply@oracle.com>
2026-04-28 07:15:31 +0000

    Updated to v3.5.3 from https://github.com/prometheus/prometheus.git
    Generated from template at a24709844b8fbf1afd71b3343fb1b250892f48bf

---
+++ olm/build-image.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+name="prometheus"
+version="3.5.3"
+registry="container-registry.oracle.com/olcne"
+docker_tag=${registry}/${name}:v${version}
+
+# Copy the promu tool from the container image
+mkdir -p bin
+podman create --pull always --name tmpcopy container-registry.oracle.com/olcne/promu:v0.17.0
+podman cp tmpcopy:/bin/promu bin/promtool
+podman rm tmpcopy
+
+podman build --pull \
+    --build-arg https_proxy=${https_proxy} \
+    --volume /etc/yum.repos.d:/etc/yum.repos.d \
+    --volume ~/.npmrc:/.npmrc \
+    -t ${docker_tag} -f ./olm/builds/Dockerfile .
+podman save -o ${name}.tar ${docker_tag}
